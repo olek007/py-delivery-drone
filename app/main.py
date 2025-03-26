25331c7d47a57d93f914b369924a33e25a4a67c4
Aleksander Rokita <kelor2@o2.pl>
2025-03-26 10:56:18 +0100

    Fix FlyingRobot coords passing to super

--- app/main.py
+++ app/main.py
@@ -35,8 +35,8 @@ class FlyingRobot(BaseRobot):
                  weight: int,
                  coords: list | None = None
                  ) -> None:
-        super().__init__(name, weight, coords)
         self.coords = coords or [0, 0, 0]
+        super().__init__(name, weight, self.coords)
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
